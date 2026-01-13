45819e3e33bf376edf85c38288b9160d39b2677d
skido <skido@jamstec.go.jp>
2026-01-13 13:11:02 +0900

    Initial commit

--- CODES/run_params.f90
+++ CODES/run_params.f90
@@ -1,7 +1,7 @@
 module run_params
   implicit none
   integer,parameter :: idx = 8,maxlen=400
-  real(idx) :: missing_value=1.0e20
+  real(idx) :: missing_value=-9999.9_idx
   real(idx),parameter :: rho0_o=1.024e3,g=9.8_idx
   real(idx),parameter :: pi = 4.0_idx *atan(1.0_idx),beta=2.28e-11_idx
   real(idx),parameter :: day_to_sec=60.0_idx * 60.0_idx * 24.0_idx  ! [s/day]
