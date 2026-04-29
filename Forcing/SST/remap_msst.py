c34a6d216b62ca039585a9f89065c1271540a6f4
skido <skido@jamstec.go.jp>
2026-04-29 14:20:32 +0900

    Update preprocessing SST files

--- Forcing/SST/remap_msst.py
+++ Forcing/SST/remap_msst.py
@@ -1,9 +1,8 @@
 import subprocess
 import glob
 
-grd_name="eqpac_v1";fname_gdes="../..//INPUT/OCN/des_grid_"+grd_name+".dat"
-grd_name="atm_256";fname_gdes="../../INPUT/ATM/des_256.txt"
-fname_in="ERSST_v5_1958_2019.nc"
+grd_name="eqpac_30";fname_gdes="../..//INPUT/OCN/des_grid_"+grd_name+".dat"
+fname_in="/Users/skido/DATA/SST/ERSST_v5_1958_2019.nc"
 fname_clm="ERSST_v5_"+str(grd_name)+"_clm.nc"
 fname_anm="ERSST_v5_"+str(grd_name)+"_anm.nc"
 
@@ -23,8 +22,8 @@ command="mv b.nc "+fname_clm
 subprocess.call(command.split())
 command="cdo ymonsub a.nc "+fname_clm+" "+fname_anm
 subprocess.call(command.split())
-command="cdo setmisstonn "+fname_anm+" b.nc"
-subprocess.call(command.split())
+#command="cdo setmisstonn "+fname_anm+" b.nc"
+#subprocess.call(command.split())
 command="mv b.nc "+fname_anm
 subprocess.call(command.split())
 command="rm a.nc"
